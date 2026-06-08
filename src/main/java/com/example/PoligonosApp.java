3ea62ffe485817471182aefc5b40258a66c0a77e
Guto <guto@yahoo.com.br>
2026-06-07 22:09:55 -0300

    Implementado o método perimetros com flatMap

--- src/main/java/com/example/PoligonosApp.java
+++ src/main/java/com/example/PoligonosApp.java
@@ -179,8 +179,14 @@ public class PoligonosApp extends Application {
     ///
     /// @return uma lista contendo o perímetro de cada polígono
     protected List<Double> perimetros(){
-        // TODO Apague esta linha e a próxima e implemente seu código
-        return List.of();
+        return pontosPoligonos.stream()
+                              .flatMap(listaPontos -> {
+                                  final var pontoInicial = listaPontos.getLast();
+                                  final var pontoFinal = listaPontos.stream()
+                                          .reduce(pontoInicial, Point::new);
+                                  return Stream.of(pontoFinal.distance());
+                              })
+                              .toList();
     }
 }
 
